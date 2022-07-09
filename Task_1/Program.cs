210f25497214619d5574a0eb21b413d038f52474
Andrey <achuklin87@gmail.com>
2022-07-09 22:29:47 +0300

    Task_Home_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Есть два числа, 10 и 55");
+int number_1 = 15;
+int number_2 = 55;
+if (number_1 < number_2)
+{
+    Console.WriteLine("Максимальное число " + number_2);
+}
+if (number_1 < number_2)
+{
+    Console.WriteLine("Минимальное число " + number_1);
+}
