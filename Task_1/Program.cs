c1858e5e915bf01f1f7e5546d2e2ee2278f08a84
Andrey <achuklin87@gmail.com>
2022-07-12 14:01:06 +0300

    Task_2

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,20 +1,42 @@
-﻿Console.WriteLine("На вход пришло два числа, 10 и 55. Из них: ");
+﻿void Zadacha1()
+{
+Console.WriteLine("На вход пришло два числа, 10 и 55. Из них: ");
 int number_1 = 15;
 int number_2 = 55;
 if (number_1 < number_2)
 {
     Console.WriteLine("Максимальное число " + number_2);
-}
-else
-{
-    Console.WriteLine("Максимальное число " + number_1);
-}
-
-if (number_1 < number_2)
-{
     Console.WriteLine("Минимальное число " + number_1);
 }
 else
 {
+    Console.WriteLine("Максимальное число " + number_1);
     Console.WriteLine("Минимальное число " + number_2);
 }
+}
+
+//Zadacha1();
+
+
+Console.WriteLine("У нас есть три числа: 7, 66, 46. Из этих трех чисел максимальным будет: ");
+int number_1 = 58;
+int number_2 = -66;
+int number_3 = -46;
+int number_max = -1;
+    
+    if(number_1 >= number_max)
+    {
+        number_max = number_1;
+    }
+    
+    if(number_2 >= number_max)
+    {
+        number_max = number_2;
+    }
+    
+    if(number_3 >= number_max)
+    {
+        number_max = number_3;
+    }
+    
+Console.WriteLine("Число " + number_max);
